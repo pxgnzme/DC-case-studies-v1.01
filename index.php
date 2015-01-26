cb1ccfdf541c6e18b44af5dd78c9688ad54fb3ff
Frazer Cameron <frazer.cameron@nzme.co.nz>
2015-01-27 12:53:20 +1300

    mock up r2

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+
+    <link href='http://fonts.googleapis.com/css?family=Raleway:100,400,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:700,400,100' rel='stylesheet' type='text/css'>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>TITLE HERE</title>
+
+    <link rel="stylesheet" href="fonts/foundation-icons.css" />
+    <link rel="stylesheet" href="css/screen.css?1" />
+
+    <script src="js/vendor/modernizr.js"></script>
+    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/custom.js?1"></script>
+
+  </head>
+
+  <body>
+
+   
+  </body>
+</html>
